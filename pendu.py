0a62261f0e096949c6ce7fb0ec88f98e16aa8047
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-08-16 16:36:45 +0200

    Implements basic hangman game

--- pendu.py
+++ pendu.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
 # -*-coding:Utf-8 -*
+
+from functions import *
+
+theWord = loadWord()
+hidden = hiddenWord(theWord)
+remainingTries = NB_TRY
+triedLetters = []
+wordDiscovered = False
+
+while(remainingTries > 0 and not wordDiscovered):
+	displayHidden(hidden)
+	letter = getLetterInput(triedLetters)
+	print(triedLetters.append(letter))
+	indexes = letterIn(letter, theWord)
+	if( not indexes[0] == -1):
+		hidden = getUpdatedHidden(hidden, letter, indexes)
+	else:
+		remainingTries -= 1
+	if(theWord == hidden):
+		wordDiscovered = True
+	print("Lettres essayées : {0}. {1} tentative(s) restante(s)".format(", ".join(triedLetters), remainingTries))
+if(wordDiscovered):
+	print("Gagné !")
+else:
+	print("perdu :(")
